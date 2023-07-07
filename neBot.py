a63b4cb579a90bc48f6b8bc7701189873578afc3
ArtemiiD <artemiidudinov@gmail.com>
2023-07-07 17:28:33 +0500

    v3

--- neBot.py
+++ neBot.py
@@ -6,6 +6,9 @@ import os
 load_dotenv(find_dotenv())
 TOKEN = os.getenv('TOKEN')
 
+
+
+
 bot = AsyncTeleBot(TOKEN, parse_mode='HTML')
 
 @bot.message_handler(commands=['start'])
@@ -14,108 +17,32 @@ async def send_welcome(message):
     await bot.send_message(chat_id, 'Привет, я неБот')
     print(message)
 
-@bot.message_handler(commands=['random'])
-async def random(message):
+#C:\Users\Algoritmika\Desktop\коллажик\банч.jpg
+
+@bot.message_handler(content_types=['text'])
+async def send_ffile(message):
     chat_id = message.from_user.id
-    bot_message = await bot.send_dice(chat_id, '🎲')
-    print(bot_message.dice.value)
-
-@bot.message_handler(commands=['contact'])
-async def contact(message):
-    chat_id = message.from_user.id
-    await bot.send_message(chat_id, '<a href="tg://user?id=5794033915">my account</a>')
-
-@bot.message_handler(commands=['timer'])
-async def timer(message):
-    chat_id = message.from_user.id
-    await bot.send_message(chat_id, 'На сколько?  (сек)')
-    time = chat_id
-    bot_message = await bot.send_message(chat_id, 'Таймер начался -', time, 'секунд')
-    for i in range(1, time):
-        await asyncio.sleep(1)
-        await bot.edit_message_text(f'{time - i} секуунд прошло', chat_id, bot_message.id)
-    await bot.delete_message(chat_id, bot_message.id)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    path_ffile = message.text
+    try:
+        await bot.send_document(chat_id, open(path_ffile,'rb'))
+    except:
+        await bot.send_message(chat_id,'Такого файла нет.')
+
+# @bot.message_handler(commands=['random'])
+# async def random(message):
+#     chat_id = message.from_user.id
+#     bot_message = await bot.send_dice(chat_id, '🎲')
+#     print(bot_message.dice.value)
+#
+# @bot.message_handler(commands=['contact'])
+# async def contact(message):
+#     chat_id = message.from_user.id
+#     await bot.send_message(chat_id, '<a href="tg://user?id=5794033915">my account</a>')
+#
+# @bot.message_handler(commands=['allfiles'])
+# async def allfiles(message):
+#     chat_id = message.from_user.id
+#     await bot.send_message(chat_id, files)
 
 
 
